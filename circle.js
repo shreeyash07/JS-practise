9b8025ed409b1ae82442ea681d029e32923761d3
shreeyash07 <45554490+shreeyash07@users.noreply.github.com>
2020-12-05 22:01:54 +0545

    Create circle.js

---
+++ circle.js
@@ -0,0 +1,11 @@
+function calcCircumfrence(radius) {
+  var circumference = (Math.PI * radius) * 2;
+console.log("The circumfrence is " + circumference + ".");
+}
+function calcArea(radius) {
+  var area = (Math.PI * radius) * radius;
+console.log("The area is " + area + ".");
+}
+
+calcCircumfrence(40);
+calcArea(20);
